5843bdbfb8e1a4580d0a7d158d4ef07d0796209d
mutasem <motasemimad10@gmail.com>
2023-02-28 14:51:53 +0200

    style title sidebar and add Videos components

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 import Sidebar from './Sidebar';
+import Videos from './Videos';
 
 function Feed() {
   return (
@@ -20,6 +21,19 @@ function Feed() {
         >
           copyright 2023 JSM Media
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography
+          variant='h4'
+          fontWeight='bold'
+          mb={2}
+          sx={{ color: 'white' }}
+        >
+          New <span style={{ color: '#FC1503' }}>videos</span>
+        </Typography>
+
+        <Videos />
       </Box>
     </Stack>
   );

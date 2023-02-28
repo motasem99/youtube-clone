ea0a383611df44066062726dc761571cf41413ac
mutasem <motasemimad10@gmail.com>
2023-02-28 16:41:14 +0200

    clear code

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -2,7 +2,6 @@ import { Link } from 'react-router-dom';
 import { Typography, Card, CardContent, CardMedia } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
 import {
-  demoThumbnailUrl,
   demoChannelTitle,
   demoChannelUrl,
   demoVideoUrl,

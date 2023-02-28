8b29f58add68ea3d9855262bcdbe9ac8f50936f1
mutasem <motasemimad10@gmail.com>
2023-02-28 16:11:03 +0200

    display video in layout demo

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import { Stack, Box } from '@mui/material';
+import { ChannelCard, VideoCard } from './';
 
 function Videos({ videos }) {
-  console.log(videos);
-
-  return <div>Videos</div>;
+  return (
+    <Stack direction='row' flexWrap='wrap' justifyContent='start' gap={2}>
+      {videos.map((item, index) => (
+        <Box key={index}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {
+            // item.id.channelId && <ChannelCard ChannelDetail={item} />
+          }
+        </Box>
+      ))}
+    </Stack>
+  );
 }
 
 export default Videos;

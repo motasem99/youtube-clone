2b8d73633fabe0e405c89e512d5b75192a6dff5e
mutasem <motasemimad10@gmail.com>
2023-03-01 16:04:29 +0200

    handle search and display data

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { Box, Typography } from '@mui/material';
+import { useParams } from 'react-router-dom';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
+import { Videos } from './';
 
 function SearchFeed() {
-  return <div>SearchFeed</div>;
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) => {
+      setVideos(data.items);
+    });
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+      <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: 'white' }}>
+        Search Results For:{' '}
+        <span style={{ color: '#FC1503' }}>{searchTerm}</span> Videos
+      </Typography>
+
+      <Videos videos={videos} />
+    </Box>
+  );
 }
 
 export default SearchFeed;
